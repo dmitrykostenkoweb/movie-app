defc1a22745aff9b31b94fe2dd5bcd702f19cebd
Dmitry Kostenko <60621984+dmitrykostenkoweb@users.noreply.github.com>
2021-11-30 23:17:50 +0100

    fix search bug, add movieListView component

--- pages/index.vue
+++ pages/index.vue
@@ -15,78 +15,15 @@
       </button>
     </div>
     <!-- loading -->
-    <Loading v-if="$fetchState.pending" />
+    <loading v-if="$fetchState.pending" />
     <!-- Movies -->
     <div class="container movies">
       <!--Searched Movies -->
+      <movie-list-view v-if="searchInput !== ''" :movies="searchedMovies" />
 
-      <div v-if="searchInput !== ''" class="movies-grid" id="movie-grid">
-        <div class="movie" v-for="(movie, idx) in searchedMovies" :key="idx">
-          <div class="movie-img">
-            <img
-              :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
-              :alt="`${movie.title} poster`"
-            />
-            <p class="review">{{ movie.vote_average }}</p>
-            <p class="overview">{{ movie.overview }}</p>
-          </div>
-          <div class="info">
-            <p class="title">
-              {{ movie.title.slice(0, 25)
-              }}<span v-if="movie.title.length > 25">...</span>
-            </p>
-            <p class="release">
-              Released:
-              {{ movie.release_date }}
-            </p>
-            <NuxtLink
-              class="button button-light"
-              :to="{
-                name: 'movies-movieid',
-                params: {
-                  movieid: movie.id,
-                },
-              }"
-            >
-              Get more info</NuxtLink
-            >
-          </div>
-        </div>
-      </div>
       <!-- Now Streaming -->
-      <div v-else class="movies-grid" id="movie-grid">
-        <div class="movie" v-for="(movie, idx) in movies" :key="idx">
-          <div class="movie-img">
-            <img
-              :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
-              :alt="`${movie.title} poster`"
-            />
-            <p class="review">{{ movie.vote_average }}</p>
-            <p class="overview">{{ movie.overview }}</p>
-          </div>
-          <div class="info">
-            <p class="title">
-              {{ movie.title.slice(0, 25)
-              }}<span v-if="movie.title.length > 25">...</span>
-            </p>
-            <p class="release">
-              Released:
-              {{ movie.release_date }}
-            </p>
-            <NuxtLink
-              class="button button-light"
-              :to="{
-                name: 'movies-movieid',
-                params: {
-                  movieid: movie.id,
-                },
-              }"
-            >
-              Get more info</NuxtLink
-            >
-          </div>
-        </div>
-      </div>
+      <movie-list-view v-else :movies="movies" />
+
       <div ref="observer" class="observer"></div>
     </div>
   </div>
@@ -95,6 +32,7 @@
 <script>
 import axios from 'axios'
 import Loading from './Loading.vue'
+import MovieListView from '../components/movieListView.vue'
 export default {
   head() {
     return {
@@ -102,7 +40,7 @@ export default {
       meta: [{ hid: 'description', name: 'description', content: 'movies' }],
     }
   },
-  components: { Loading },
+  components: { Loading, MovieListView },
   data() {
     return {
       movies: [],
@@ -119,7 +57,7 @@ export default {
 
     await this.searchMovies()
   },
-  fetchDelay: 300,
+  fetchDelay: 500,
   mounted() {
     this.getMoreMovies()
   },
@@ -137,6 +75,8 @@ export default {
       }
     },
     async searchMovies() {
+      this.searchedMovies = []
+
       try {
         const responseSearchedMovies = await axios.get(
           `https://api.themoviedb.org/3/search/movie?api_key=aafd5624e29d6f2b8ceb629bd5243be0&language=en-US&query=${this.searchInput}&page=1&include_adult=false`
@@ -170,7 +110,7 @@ export default {
   },
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .loading {
   padding-top: 120px;
   align-items: flex-starts;
@@ -193,85 +133,5 @@ export default {
 // .....
 .movies {
   padding: 32px 16px;
-  .movies-grid {
-    display: grid;
-    column-gap: 32px;
-    row-gap: 64px;
-    grid-template-columns: 1fr;
-    @media (min-width: 500px) {
-      grid-template-columns: repeat(2, 1fr);
-    }
-    @media (min-width: 750px) {
-      grid-template-columns: repeat(2, 1fr);
-    }
-    @media (min-width: 1100px) {
-      grid-template-columns: repeat(4, 1fr);
-    }
-    .movie {
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      .movie-img {
-        position: relative;
-        overflow: hidden;
-        &:hover {
-          .overview {
-            transform: translateY(0);
-          }
-        }
-        img {
-          display: block;
-          width: 100%;
-          height: 100%;
-        }
-        .review {
-          position: absolute;
-          top: 0;
-          left: 0;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width: 40px;
-          height: 40px;
-          background-color: #c92502;
-          color: #fff;
-          border-radius: 0 0 16px 0;
-          box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-            0 2px 4px -1px rgba(0, 0, 0, 0.06);
-        }
-        .overview {
-          line-height: 1.5;
-          position: absolute;
-          bottom: 0;
-          background-color: rgba(201, 38, 2, 0.5);
-          padding: 12px;
-          color: #fff;
-          transform: translateY(100%);
-          transition: 0.3s ease-in-out all;
-        }
-      }
-      .info {
-        margin-top: auto;
-        .title {
-          margin-top: 8px;
-          color: #fff;
-          font-size: 20px;
-        }
-        .release {
-          margin-top: 8px;
-          color: #c9c9c9;
-        }
-        .button {
-          margin-top: 8px;
-        }
-      }
-    }
-  }
 }
-
-// .observer {
-//   width: 100%;
-//   height: 50px;
-//   background-color: #c92502;
-// }
 </style>
